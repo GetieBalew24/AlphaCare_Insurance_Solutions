ce4cf79a978c2f96be1ffbeab3198d2879927a5b
GetieBalew24 <getiebalew20@gmail.com>
2024-12-27 20:48:40 +0300

    EDA Univariate Analysis

--- scripts/alphacare_insurance_EDA.py
+++ scripts/alphacare_insurance_EDA.py
@@ -15,3 +15,24 @@ class InsuranceEDA:
         print("Data Summary:\n", df.describe())
         print("\nData Types:\n", df.dtypes)
         print("\nMissing Values:\n", df.isnull().sum())
+     # Univariate Analysis: Plotting histograms and bar charts
+    def plot_histograms(self,df, numerical_cols):
+        """
+        Plots histograms for numerical columns.
+        """
+        for col in numerical_cols:
+            plt.figure(figsize=(10, 6))
+            sns.histplot(df[col].dropna(), bins=30, kde=True)
+            plt.title(f"Distribution of {col}")
+            plt.xticks(rotation=90, ha='right')
+            plt.show()
+    def plot_bar_charts(self,df,categorical_cols):
+        """
+        Plots bar charts for categorical columns.
+        """
+        for col in categorical_cols:
+            plt.figure(figsize=(10, 6))
+            sns.countplot(x=col, data=df)
+            plt.title(f"Distribution of {col}")
+            plt.xticks(rotation=90, ha='right')
+            plt.show()
